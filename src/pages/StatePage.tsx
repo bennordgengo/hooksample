7288fa7e1e1eef3f1628ba5112400dff863b2699
Ben Nordstrom <ben.nordstrom@gengo.com>
2020-02-13 14:53:21 +0900

    state comments

--- src/pages/StatePage.tsx
+++ src/pages/StatePage.tsx
@@ -17,26 +17,24 @@ interface CustomButtonProps {
 const CustomButton: React.FC<CustomButtonProps> = props => {
   console.log("CustomButton rendering...");
   console.log(props);
+
   return <IonButton onClick={props.onClick}>{props.children}</IonButton>;
-};
-
-const Panel: React.FC = props => {
-  console.log("Panel rendering...");
-  console.log(props);
-
-  const [text, setText] = useState<string>("My initial string");
-
-  return (
-    <>
-      <p>{text}</p>
-      <CustomButton onClick={() => setText("Hello")}>Change text to Hello</CustomButton>
-      <CustomButton onClick={() => setText("Good bye")}>Change text to Good bye</CustomButton>
-    </>
-  );
 };
 
 const StatePage: React.FC = () => {
   console.log("StatePage rendering...");
+
+  const [text, setText] = useState<string>("My initial string");
+
+  // DON'T setText in the body of the function directly
+  //
+  // That just causes the React.FC to need to be rendered again immediately with the new value
+  // setText("some new string"):
+  //
+  // This includes setting state based on other state in the function
+  // const [longText, setLongText] = useState<string>("");
+  // setLongText(text + "suffix");
+
   return (
     <IonPage>
       <IonHeader>
@@ -48,7 +46,9 @@ const StatePage: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <Panel />
+        <p>{text}</p>
+        <CustomButton onClick={() => setText("Hello")}>Change text to Hello</CustomButton>
+        <CustomButton onClick={() => setText("Good bye")}>Change text to Good bye</CustomButton>
       </IonContent>
     </IonPage>
   );
